bb4c9caca5e05df36c2ccd49e4846576bebd9105
julianeon <julianeon@gmail.com>
2023-10-26 13:06:33 -0700

    input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,146 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+type FormData = {
+  name: string;
+  email: string;
+  phone: string;
+  problem: string;
+};
+
+const API_ENDPOINT="https://x0x0.execute-api.us-east-1.amazonaws.com/production";
+
+const Form: React.FC = () => {
+  const [note, setNote] = useState<string>("...");
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    phone: "",
+    problem: "",
+  });
+
+  const [errors, setErrors] = useState<Partial<FormData>>({});
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const validateForm = () => {
+    const newErrors: Partial<FormData> = {};
+
+    // Basic validation, you can add more specific validation here if needed
+    if (!formData.name) {
+      newErrors.name = "Name is required.";
+    }
+    if (!formData.email) {
+      newErrors.email = "Email is required.";
+    }
+    if (!formData.phone) {
+      newErrors.phone = "Phone is required.";
+    } else if (!/^[0-9]*$/.test(formData.phone)) {
+      newErrors.phone = "Please enter a valid phone number, all numbers.";
+    }
+    if (!formData.problem) {
+      newErrors.problem = "Nature of Problem is required.";
+    }
+
+    setErrors(newErrors);
+
+    // Return true if there are no errors
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const isValid = validateForm();
+
+    if (isValid) {
+      try {
+        const response = await axios.post(API_ENDPOINT, formData);
+
+        if (response.status === 200) {
+          // Successful submission, you can add a thank you message here
+          setNote("Thank you for submitting the form! We've received your submission and will be in touch.");
+        }  else { 
+          setNote(response.status.toString());
+      }
+    } catch (error) {
+      setNote("There was an error submitting the form, but that's okay if you're not using a real API endpoint. If you were & it worked, you would've seen a 'thank you' here.");
+    }
+    };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  <div>
+    <form onSubmit={handleSubmit}>
+      <div>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <label htmlFor="name">Name:</label>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>
+          <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+          />
+        </p>
+        <p>{errors.name && <span>{errors.name}</span>}</p>
+      </div>
+      <div>
+        <p>
+          <label htmlFor="email">Email:</label>
+        </p>
+        <p>
+          <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
+        </p>
+        <p>{errors.email && <span>{errors.email}</span>}</p>
+      </div>
+      <div>
+        <p>
+          <label htmlFor="phone">Phone:</label>
+        </p>
+        <p>
+          <input
+          type="tel"
+          id="phone"
+          name="phone"
+          value={formData.phone}
+          onChange={handleInputChange}
+          />
+        </p>
+        <p>{errors.phone && <span>{errors.phone}</span>}</p>
+      </div>
+      <div>
+        <p>
+          <label htmlFor="problem">Nature of Problem:</label>
+        </p>
+        <p>
+          <textarea
+          id="problem"
+          name="problem"
+          value={formData.problem}
+          onChange={handleInputChange}
+          />
+        </p>
+        <p>{errors.problem && <span>{errors.problem}</span>}</p>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+    <p>{note}</p>
+  </div>
   );
-}
+};
 
-export default App;
+export default Form;
